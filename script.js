bbf568a7e687993a50674b7678b3ee9a24704813
S57863B <i349cjhswe@gmail.com>
2026-03-27 16:01:51 -0400

    Initialize file and select DOM elements

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // 1. Select the main form element to handle the submit event later
+    const form = document.getElementById('registrationForm');
+
+    // 2. Select all the input fields where the user will type their data
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+
+    // 3. Select all the span elements where program will inject custom error messages
+    const usernameError = document.getElementById('usernameError');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    const confirmPasswordError = document.getElementById('confirmPasswordError');
+});
